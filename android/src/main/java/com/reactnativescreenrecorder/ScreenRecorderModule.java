dc97f451ec5423c8cff6b5d6f2871402ce338943
chien.vu <vuvanchien@Vus-MacBook-Pro.local>
2023-06-28 11:06:31 +0700

    init commit

--- android/src/main/java/com/reactnativescreenrecorder/ScreenRecorderModule.java
+++ android/src/main/java/com/reactnativescreenrecorder/ScreenRecorderModule.java
@@ -1,3 +1,35 @@
+package com.reactnativescreenrecorder;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+
+import android.os.Build;
+import android.util.Log;
+import android.content.Intent;
+import android.media.projection.MediaProjectionManager;
+import android.content.Context;
+import android.app.Activity;
+
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.BaseActivityEventListener;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.Arguments;
+
+import com.facebook.react.module.annotations.ReactModule;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+import com.hbisoft.hbrecorder.HBRecorder;
+import com.hbisoft.hbrecorder.HBRecorderListener;
+
+import java.io.File;
+
+@ReactModule(name = ScreenRecorderModule.NAME)
+public class ScreenRecorderModule extends ReactContextBaseJavaModule implements HBRecorderListener {
   public static final String NAME = "ScreenRecorder";
   private final ReactApplicationContext reactContext;
   private final int SCREEN_RECORD_REQUEST_CODE = 1000;
@@ -24,7 +56,7 @@
 
         if (resultCode == Activity.RESULT_OK) {
           Log.d("ScreenRecorder", "User accepted permission");
-          hbRecorder.startScreenRecording(intent, resultCode);
+          hbRecorder.startScreenRecording(intent, resultCode, getCurrentActivity());
         }
       }
     });
@@ -47,6 +79,10 @@
     hbRecorder.isAudioEnabled(!config.hasKey("mic") || (boolean) config.getBoolean("mic"));
     hbRecorder.setVideoEncoder("DEFAULT");
     hbRecorder.setOutputPath(outputUri.toString());
+
+    boolean notificationActionEnabled = config.hasKey("notificationActionEnabled") && (boolean) config.getBoolean("notificationActionEnabled");
+    hbRecorder.setNotificationActionEnabled(notificationActionEnabled);
+    if (!notificationActionEnabled) hbRecorder.setNotificationDescription("Stop recording from the application");
 
     try{ // Requesting user permissions
       MediaProjectionManager mediaProjectionManager = (MediaProjectionManager) reactContext.getSystemService (Context.MEDIA_PROJECTION_SERVICE);
@@ -100,13 +136,4 @@
     Log.d("ScreenRecorder", "HBRecorderOnError : " + errorCode + " " + reason);
     startPromise.reject("404", "RecorderOnError:" + errorCode + " " + reason);
   }
-
-  @Override
-  public void HBRecorderOnPause() {
-
-  }
-
-  @Override
-  public void HBRecorderOnResume() {
-
-  }
+}
